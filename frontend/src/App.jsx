d13380cae5546b663bcb68f9874c3aefc13ea0e1
anindita-01 <deyanindita198@gmail.com>
2026-05-28 23:32:02 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Doctors from './pages/Doctors.jsx'
+import Login from './pages/Login.jsx'
+import Contact from './pages/Contact.jsx'
+import About from './pages/About.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import Myappointments from './pages/Myappointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/doctors' element={<Doctors/>}/>
+      <Route path='/doctors/:speciality' element={<Doctors/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/my-profile' element={<MyProfile/>}/>
+      <Route path='/my-appointments' element={<Myappointments/>}/>
+      <Route path='/my-appointments/:docId' element={<Appointment/>}/>
+     </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-xl text-red-800'>
+      dabckBD,kjAHDKJHQKAsdh
+    </div>
+  )
+}
+
+export default Home
